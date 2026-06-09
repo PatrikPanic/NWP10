68822724ff0e9ecfc5440163689f26924123039d
PatrikPanic <papanic@vsite.hr>
2026-06-09 14:51:18 +0200

    V010

--- RecView.cpp
+++ RecView.cpp
@@ -20,19 +20,19 @@ static char THIS_FILE[] = __FILE__;
 IMPLEMENT_DYNCREATE(RecView, CRecordView)
 
 BEGIN_MESSAGE_MAP(RecView, CRecordView)
-	//{{AFX_MSG_MAP(RecView)
-	//}}AFX_MSG_MAP
-	// Standard printing commands
-	ON_COMMAND(ID_FILE_PRINT, CRecordView::OnFilePrint)
-	ON_COMMAND(ID_FILE_PRINT_DIRECT, CRecordView::OnFilePrint)
-	ON_COMMAND(ID_FILE_PRINT_PREVIEW, CRecordView::OnFilePrintPreview)
+    //{{AFX_MSG_MAP(RecView)
+    //}}AFX_MSG_MAP
+    // Standard printing commands
+    ON_COMMAND(ID_FILE_PRINT, CRecordView::OnFilePrint)
+    ON_COMMAND(ID_FILE_PRINT_DIRECT, CRecordView::OnFilePrint)
+    ON_COMMAND(ID_FILE_PRINT_PREVIEW, CRecordView::OnFilePrintPreview)
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // RecView construction/destruction
 
 RecView::RecView()
-	: CRecordView(RecView::IDD)
+    : CRecordView(RecView::IDD)
 {
 }
 
@@ -42,22 +42,25 @@ RecView::~RecView()
 
 void RecView::DoDataExchange(CDataExchange* pDX)
 {
-	CRecordView::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(RecView)
-	//}}AFX_DATA_MAP
+    CRecordView::DoDataExchange(pDX);
+    //{{AFX_DATA_MAP(RecView)
+    //}}AFX_DATA_MAP
+    DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+    DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
+    DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
 }
 
 BOOL RecView::PreCreateWindow(CREATESTRUCT& cs)
 {
-	return CRecordView::PreCreateWindow(cs);
+    return CRecordView::PreCreateWindow(cs);
 }
 
 void RecView::OnInitialUpdate()
 {
-	m_pSet = &GetDocument()->m_set;
-	CRecordView::OnInitialUpdate();
-	GetParentFrame()->RecalcLayout();
-	ResizeParentToFit();
+    m_pSet = &GetDocument()->m_set;
+    CRecordView::OnInitialUpdate();
+    GetParentFrame()->RecalcLayout();
+    ResizeParentToFit();
 
 }
 
@@ -66,8 +69,8 @@ void RecView::OnInitialUpdate()
 
 BOOL RecView::OnPreparePrinting(CPrintInfo* pInfo)
 {
-	// default preparation
-	return DoPreparePrinting(pInfo);
+    // default preparation
+    return DoPreparePrinting(pInfo);
 }
 
 void RecView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
@@ -84,18 +87,18 @@ void RecView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 #ifdef _DEBUG
 void RecView::AssertValid() const
 {
-	CRecordView::AssertValid();
+    CRecordView::AssertValid();
 }
 
 void RecView::Dump(CDumpContext& dc) const
 {
-	CRecordView::Dump(dc);
+    CRecordView::Dump(dc);
 }
 
 Doc* RecView::GetDocument() // non-debug version is inline
 {
-	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Doc)));
-	return (Doc*)m_pDocument;
+    ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Doc)));
+    return (Doc*)m_pDocument;
 }
 #endif //_DEBUG
 
@@ -103,10 +106,55 @@ Doc* RecView::GetDocument() // non-debug version is inline
 // RecView database support
 CRecordset* RecView::OnGetRecordset()
 {
-	return m_pSet;
+    return m_pSet;
 }
 
 
 /////////////////////////////////////////////////////////////////////////////
 // RecView message handlers
 
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo) {
+    const int pageWidth = pDC->GetDeviceCaps(HORZRES);
+
+    CFont printFont;
+    printFont.CreatePointFont(100, _T("Times New Roman"), pDC);
+    CFont* pOldFont = pDC->SelectObject(&printFont);
+
+    const CSize textSize = pDC->GetTextExtent(_T("Sample"));
+    const int margin = textSize.cx;
+    const int rowHeight = textSize.cy * 3;
+    int y = margin * 2;
+
+    const int colIdX = pageWidth / 12;
+    const int colNameX = colIdX * 3;
+    const int colManagerX = colIdX * 7;
+    const int colEndX = pageWidth - margin;
+
+    pDC->DrawText(_T("ID"), CRect(colIdX, y, colNameX, y + rowHeight), DT_LEFT);
+    pDC->DrawText(_T("Name"), CRect(colNameX, y, colManagerX, y + rowHeight), DT_LEFT);
+    pDC->DrawText(_T("Manager"), CRect(colManagerX, y, colEndX, y + rowHeight), DT_LEFT);
+
+    y += rowHeight;
+    pDC->MoveTo(margin, y);
+    pDC->LineTo(colEndX, y);
+    y += textSize.cy;
+
+    Set recordSet;
+    recordSet.Open();
+
+    while (!recordSet.IsEOF()) {
+        CString idStr;
+        idStr.Format(_T("%d"), recordSet.m_id);
+
+        pDC->DrawText(idStr, CRect(colIdX, y, colNameX, y + rowHeight), DT_LEFT);
+        pDC->DrawText(recordSet.m_name, CRect(colNameX, y, colManagerX, y + rowHeight), DT_LEFT);
+
+        CString managerStr = recordSet.m_manager ? _T("Yes") : _T("No");
+        pDC->DrawText(managerStr, CRect(colManagerX, y, colEndX, y + rowHeight), DT_LEFT);
+
+        y += rowHeight;
+        recordSet.MoveNext();
+    }
+
+    pDC->SelectObject(pOldFont);
+}
